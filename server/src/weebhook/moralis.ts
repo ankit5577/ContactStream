e3162d2b3069815fe501633ca15b98c68c4084ea
Ankit Kaushal <kaushalankit101010@gmail.com>
2024-10-10 14:56:37 +0530

    decode events

--- server/src/weebhook/moralis.ts
+++ server/src/weebhook/moralis.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Post, Res } from '@nestjs/common';
-import { Response } from 'express'; // Import Response from express for HTTP responses
+import { ethers } from 'ethers';
+import { Response } from 'express';
 import { ContactService } from '../contact/contact.service';
 import { CreateContactDto } from '../contact/dto/create-contact-dto';
 
@@ -9,17 +10,47 @@ export class WebhookController {
 
   @Post('moralis')
   async handleEvent(@Body() body: any, @Res() res: Response) {
-    console.log('MORALIS::', body);
-
     if (!body.logs || body.logs.length === 0) {
       console.log('Received test webhook from Moralis');
       return res.status(200).send('Test webhook received');
     }
 
-    const logs = body.logs[0];
-    const eventData = logs.data;
+    const eventABI = [
+      {
+        anonymous: false,
+        inputs: [
+          {
+            indexed: false,
+            internalType: 'string',
+            name: 'name',
+            type: 'string',
+          },
+          {
+            indexed: false,
+            internalType: 'string',
+            name: 'email',
+            type: 'string',
+          },
+          {
+            indexed: false,
+            internalType: 'uint256',
+            name: 'phone',
+            type: 'uint256',
+          },
+        ],
+        name: 'ContactCreated',
+        type: 'event',
+      },
+    ];
 
-    const { name, email, phone } = eventData;
+    const logs = body.logs[0];
+
+    const iface = new ethers.utils.Interface(eventABI);
+    const decodedEvent = iface.decodeEventLog('ContactCreated', logs.data);
+
+    const { name, email, phone } = decodedEvent;
+
+    console.log('Decoded Event:', { name, email, phone });
 
     const contactDto: CreateContactDto = {
       name,
